b3ac48f52d76c68418488ed90809202de3e2d58f
iskandert <alex.tol1998@gmail.com>
2021-11-10 04:24:01 +0300

    add adaptive layout of calculating page

--- src/App.js
+++ src/App.js
@@ -1,57 +1,122 @@
 import React from 'react';
+// import { useEffect } from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
-import './App.css';
+import Button from '@mui/material/Button';
+import { AppBar, Container, CssBaseline, Grid, Link, Paper, ThemeProvider, Typography, } from '@mui/material';
+import { DataUsage } from '@mui/icons-material';
+import { Box } from '@mui/system';
+// import './App.css';
+// import calculate from './features/calculating/calculate';
+
+const containerStyle = {
+  maxWidth: {
+    lg: '900px',
+    xl: '1200px'
+  },
+}
+
 
 function App() {
+  // calculate();
+  const asideRef = React.createRef();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+    <>
+      <CssBaseline />
+      {/* <ThemeProvider theme> */}
+
+      <AppBar
+        elevation={3}
+        sx={{ py: 0.1 }}
+        position='fixed'>
+        <Container fixed sx={{
+          ...containerStyle,
+          color: 'white',
+          alignItems: 'center',
+          display: 'flex',
+          justifyContent: 'space-between',
+        }}>
+          <Link href='#'
+            // underline="none"
+            sx={{
+              color: 'inherit',
+              display: 'flex',
+              alignItems: 'center',
+              fontSize: 30,
+              // flexGrow: 1,
+            }}>
+            <DataUsage fontSize='inherit' />
+            <Typography ml={1} fontSize='inherit'>
+              Split It!
+            </Typography>
+          </Link>
+          <Button variant='outlined' sx={{
+            color: 'inherit',
+            borderColor: '#D6DBF6',
+            ':hover': {
+              // borderColor: '#9DABF6',
+              // color: '#D6DBF6',
+              borderColor: '#fff',
+            },
+          }}>New calculating</Button>
+          {/* <Button variant="text" >Text</Button>
+          <Button variant="contained">Contained</Button>
+          <Button variant="outlined">Outlined</Button> */}
+        </Container>
+      </AppBar>
+
+      <Container fixed sx={{ ...containerStyle }}>
+
+        <Grid container columns={{ xl: 4, lg: 3, md: 3, sm: 2, xs: 1 }} spacing={1} mt={6} >
+          <Grid item xl={3} lg={2} md={2} sm={2} xs={1} mb={{ xs: 2, md: 4 }} >
+            <Grid container spacing={1} columns={{ xl: 3, lg: 2, md: 2, sm: 1, xs: 1 }} >
+              <Grid item xl={1} lg={1} md={1} sm={1} xs={1} sx={{
+                display: {
+                  md: 'none',
+                }
+              }} ><Paper sx={{ height: 300, }} elevation={6} >aaa</Paper></Grid>
+              {Array.from({ length: 10 }, (_, i) => <Grid item xl={1} lg={1} md={1} sm={1} xs={1}>
+                <Paper sx={{ height: 200 }} elevation={6} >{i}</Paper>
+              </Grid>)}
+            </Grid>
+          </Grid>
+          <Grid item xl lg md sm xs sx={{
+            display: {
+              xs: 'none',
+              md: 'block',
+            }
+          }}>
+            <Box component="div" sx={{
+              mt: -7,
+              position: 'sticky',
+              top: '0',
+              height: '100vh',
+              paddingTop: 7,
+              paddingBottom: 4,
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'space-between',
+            }}>
+              <Paper sx={{ height: 300, }} elevation={6} ></Paper>
+              <Button sx={{}} variant='contained' size='large' >calculate</Button>
+            </Box>
+          </Grid>
+        </Grid>
+        <Button sx={{
+          display: {
+            xs: 'block',
+            md: 'none',
+          },
+          position: 'fixed',
+          bottom: 40,
+          left: '50%',
+          transform: 'translate(-50%, 0)',
+        }} variant='contained' size='large' >calculate</Button>
+
+      </Container>
+      {/* </ThemeProvider> */}
+    </>
   );
 }
 
--- src/features/calculating/calculate.js
+++ src/features/calculating/calculate.js
@@ -1,102 +1,114 @@
-let members = [], actions = [];
+function calculate() {
 
-let isEnterFinished = false;
-function Member(name, sum) {
-    this.name = name;
-    // this.sum = sum;
-    this.balance = sum;
-}
+    let members = [], actions = [];
 
-console.log("Enter name of one and set of his costs\nor press Enter if that's all: ");
-while (!isEnterFinished) {
-    const action = prompt("Enter name of one and set of his costs\nor press Enter if that's all: ", '');
-    if (action) {
-        action = action.split(' ');
-        actions.push([action[0], +action[1], action.slice(2) || []]);
+    let isEnterFinished = false;
+    function Member(name, sum) {
+        this.name = name;
+        // this.sum = sum;
+        this.balance = sum;
     }
-    else isEnterFinished = true;
-}
 
-if (!actions.length) actions = [
-    ['A', 25, []],
-    ['B', 15, ['A', 'C']],
-    ['B', 20, []],
-    ['C', 34, []],
-    ['D', 40, []],
-    ['E', 20, ['A', 'B', 'D', 'F', 'G', 'H', 'I', 'J']],
-    ['F', 15, []],
-    ['G', 25, []],
-    ['H', 31, []],
-    ['I', 35, []],
-    ['J', 10, []],
-];
-
-let a = new Date().getMilliseconds();
-actions.forEach(e => { e[2] = e[2].sort().join('&') });
-let costGroups = new Map(), names = Array.from(new Set(actions.map(e => e[0])));
-
-actions.forEach(e => {
-    !members.map(m => m.name).includes(e[0]) ?
-        members.push(new Member(e[0], e[1]))
-        : members.find(m => {
-            if (m.name !== e[0]) return false;
-            m.sum += e[1];
-            m.balance += e[1];
-            return true;
-        });
-    if (!costGroups.size || !Array.from(costGroups.keys()).includes(e[2])) costGroups.set(e[2], undefined);
-});
-
-Array.from(costGroups.keys()).forEach(g => {
-    costGroups.set(g, actions.filter(e => e[2] === g).map(e => e.slice(0, 2)));
-    if (new Set(costGroups.get(g).map(c => c[0])).size < names.length - (g.split('&').join('').length ? g.split('&').length : 0)) {
-        const list = costGroups.get(g);
-        costGroups.set(g, [
-            ...list,
-            ...names.filter(n => !list.map(l => l[0]).includes(n) && !g.includes(n)).map(n => [n, 0]),
-        ]);
+    console.log("Enter name of one and set of his costs\nor press Enter if that's all: ");
+    while (!isEnterFinished) {
+        const action = prompt("Enter name of one and set of his costs\nor press Enter if that's all: ", '');
+        if (action) {
+            action = action.split(' ');
+            let a;
+            if (action.slice(2)) a = action.slice(2);
+            else a = [];
+            actions.push([action[0], +action[1], a]);
+        }
+        else isEnterFinished = true;
     }
-});
 
-Array.from(costGroups.values()).forEach(g => {
-    const meanCost = g.reduce((sum, curr) => sum + curr[1], 0) / g.length;
-    g.forEach(i => members.forEach(m => {
-        if (i[0] === m.name) m.balance = Math.trunc((m.balance - meanCost) * 1000) / 1000;
-    }));
-});
+    if (!actions.length) actions = [
+        ['A', 25, []],
+        ['B', 15, ['A', 'C']],
+        ['B', 20, []],
+        ['C', 34, []],
+        ['D', 40, []],
+        ['E', 20, ['A', 'B', 'D', 'F', 'G', 'H', 'I', 'J']],
+        ['F', 15, []],
+        ['G', 25, []],
+        ['H', 31, []],
+        ['I', 35, []],
+        ['J', 10, []],
+    ];
 
-let debtors = new Set(), creditors = new Set(), transactions = [];
-members.sort((a, b) => b.balance - a.balance).forEach(m => {
-    if (m.balance < 0) debtors.add(m);
-    if (m.balance > 0) creditors.add(m);
-});
+    let a = new Date().getMilliseconds();
+    actions.forEach(e => { e[2] = e[2].sort().join('&') });
+    let costGroups = new Map(), names = Array.from(new Set(actions.map(e => e[0])));
 
-let round = n => Math.round(n * 10) / 10;
-
-debtors.forEach(d => {
-    creditors.forEach(c => {
-        if (d.balance + c.balance !== 0) return;
-        transactions.push([d.name, c.name, round(c.balance)]);
-        d.balance = 0, c.balance = 0;
-        debtors.delete(d), creditors.delete(c);
+    actions.forEach(e => {
+        !members.map(m => m.name).includes(e[0]) ?
+            members.push(new Member(e[0], e[1]))
+            : members.find(m => {
+                if (m.name !== e[0]) return false;
+                m.sum += e[1];
+                m.balance += e[1];
+                return true;
+            });
+        if (!costGroups.size || !Array.from(costGroups.keys()).includes(e[2])) costGroups.set(e[2], undefined);
     });
-});
 
-debtors.forEach(d => {
-    if (d.balance === 0) return;
-    creditors.forEach(c => {
-        if (!d.balance || !c.balance) return;
-        if (d.balance + c.balance <= 0) {
-            transactions.push([d.name, c.name, round(c.balance)]);
-            if (d.balance + c.balance === 0) d.balance = 0;
-            else d.balance += c.balance;
-            c.balance = 0;
-        } else {
-            transactions.push([d.name, c.name, Math.abs(round(d.balance))]);
-            c.balance += d.balance;
-            d.balance = 0;
+    Array.from(costGroups.keys()).forEach(g => {
+        costGroups.set(g, actions.filter(e => e[2] === g).map(e => e.slice(0, 2)));
+        let a;
+        if (g.split('&').join('').length) a = g.split('&').length;
+        else a = 0;
+        if (new Set(costGroups.get(g).map(c => c[0])).size < names.length - a) {
+            const list = costGroups.get(g);
+            costGroups.set(g, [
+                ...list,
+                ...names.filter(n => !list.map(l => l[0]).includes(n) && !g.includes(n)).map(n => [n, 0]),
+            ]);
         }
     });
-});
-console.log((new Date().getMilliseconds() - a) / 1000);
-console.log(transactions);
+
+    Array.from(costGroups.values()).forEach(g => {
+        const meanCost = g.reduce((sum, curr) => sum + curr[1], 0) / g.length;
+        g.forEach(i => members.forEach(m => {
+            if (i[0] === m.name) m.balance = Math.trunc((m.balance - meanCost) * 1000) / 1000;
+        }));
+    });
+
+    let debtors = new Set(), creditors = new Set(), transactions = [];
+    members.sort((a, b) => b.balance - a.balance).forEach(m => {
+        if (m.balance < 0) debtors.add(m);
+        if (m.balance > 0) creditors.add(m);
+    });
+
+    let round = n => Math.round(n * 10) / 10;
+
+    debtors.forEach(d => {
+        creditors.forEach(c => {
+            if (d.balance + c.balance !== 0) return;
+            transactions.push([d.name, c.name, round(c.balance)]);
+            d.balance = 0;
+            c.balance = 0;
+            debtors.delete(d);
+            creditors.delete(c);
+        });
+    });
+
+    debtors.forEach(d => {
+        if (d.balance === 0) return;
+        creditors.forEach(c => {
+            if (!d.balance || !c.balance) return;
+            if (d.balance + c.balance <= 0) {
+                transactions.push([d.name, c.name, round(c.balance)]);
+                if (d.balance + c.balance === 0) d.balance = 0;
+                else d.balance += c.balance;
+                c.balance = 0;
+            } else {
+                transactions.push([d.name, c.name, Math.abs(round(d.balance))]);
+                c.balance += d.balance;
+                d.balance = 0;
+            }
+        });
+    });
+    console.log((new Date().getMilliseconds() - a) / 1000);
+    console.log(transactions);
+}
+export default calculate;
